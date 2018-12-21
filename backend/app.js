e6316e9f3385e04069678b275b719ce76133feba
Jelle <geers.jelle@gmail.com>
2018-12-21 18:11:05 +0100

    possible to add Passengers to a ride

--- backend/app.js
+++ backend/app.js
@@ -41,5 +41,4 @@ console.log(err.message)
   res.status(err.status || 500);
   res.send('error');
 });
-
 module.exports = app;
--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,10 +1,12 @@
 let mongoose = require('mongoose');
 let Ride= mongoose.model('Ride');
 let User = mongoose.model("User")
+let Passenger = mongoose.model("Passenger")
 var express = require('express');
 var router = express.Router();
 
 router.get('/',function(req, res, next){
+  
     User.aggregate([
       {
         "$project":{
@@ -27,6 +29,12 @@ router.get('/',function(req, res, next){
 router.get("/:username/rides",function(req,res,next){
   res.json(req.user.rides);
 });
+
+router.get("/:rideId/passengers",function(req,res,next){
+  //res.json(req.ride);
+});
+
+
 router.post('/:username/rides', function (req, res, next){
   let user =req.user;
   user.rides.push(new Ride(req.body));
@@ -51,4 +59,44 @@ router.param("username", function(req, res, next, id) {
     return next();
   });
 });
+
+//post
+//rideId
+//body: userId
+router.post("/:uid/:riid", function(req, res, next){
+  User.findOneAndUpdate(
+    {
+      _id: req.params.uid
+     },
+     {
+       $push: {
+         "rides.$[a].passengers": new Passenger({name: "RETARD "})
+       }
+     },
+     {
+       new: true,
+       arrayFilters: [{'a._id': mongoose.Types.ObjectId(req.params.riid)}]
+     }
+).exec(function(err, user){
+  console.log(user)
+  res.json("test")
+})
+});
+
+router.param("rideId",function(req, res, next, id){
+  let query= Ride.findById(id);
+  
+  query.exec(function (err,ride){
+    if (err){
+      return next(err);
+    }
+    if(!ride){
+      return next( new Error('not found '+ id));
+    }
+    req.ride= ride;
+    return next();
+  });
+});
+
+
 module.exports = router;
